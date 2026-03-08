94b3343bc420385ec958d4e70fceac6ccd12cdfc
Deepak Chouhan <69458937+deepak-chouhan@users.noreply.github.com>
2026-03-08 15:34:17 +0530

    Initliazed Teams Endpoint

--- api/src/api/v1/index.ts
+++ api/src/api/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth/auth.routes.js";
+import teamsRouter from "./teams/teams.routes.js";
 
 const V1Router = Router();
 
 V1Router.use("/auth", authRouter);
+V1Router.use("/teams", teamsRouter);
 
 export default V1Router;
