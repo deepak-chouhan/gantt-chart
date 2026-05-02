806cfb13482dc19a6c787ea97e8ed6c272752fef
Deepak Chouhan <69458937+deepak-chouhan@users.noreply.github.com>
2026-05-02 10:31:40 +0530

    Updated fileSize key for multer to limit file size

--- api/src/api/v1/jobs/jobs.controller.ts
+++ api/src/api/v1/jobs/jobs.controller.ts
@@ -9,7 +9,7 @@ import { ErrorCode, HttpStatus } from "../../../types/error.types.js";
 
 export const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fieldSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 5 * 1024 * 1024 },
   fileFilter: (_req, file, cb) => {
     const ext = path.extname(file.originalname).toLowerCase();
     if (file.mimetype !== "text/csv" || ext !== ".csv") {
